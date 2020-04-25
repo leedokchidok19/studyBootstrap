8d1cb32cc879eec732c9cc56ab3872e4cac2f78a
leedokchidok19 <duckhyun4433@naver.com>
2020-04-25 21:20:19 +0900

    동적 목록 생성

---
+++ main/index.js
@@ -0,0 +1,20 @@
+$(function(){
+  document.cookie = 'cross-site-cookie=bar; SameSite=Lax';
+
+  //IIFE
+  (function studyList(){
+    let htmlTitle = ['부트스트랩 개념', '디바이스별 크기', 'col-size-number', '레이아웃 실습'];
+
+    htmlTitle.forEach(function(e, i) {
+      let htmlNumber = i+1;
+
+        if(htmlNumber<10) htmlNumber = "0" + htmlNumber;
+
+        let childTag = '<div class="col-xs-12"><a href="./main/ex'+htmlNumber+'.html">'+e+'</a></div>';
+        $('#studyList').append(childTag);
+
+    });//forEach end
+
+  })();// IIFE
+
+});//function end
