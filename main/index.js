e0a37a7ea128c18f4444e42a8452ca677ce2d807
leedokchidok19 <duckhyun4433@naver.com>
2020-04-27 22:34:56 +0900

    이미지, helper 클래스, 디바이스 크기에 따른 보이기숨기기, 글리피콘, 드랍다운, 네빅이션

--- main/index.js
+++ main/index.js
@@ -3,7 +3,12 @@ $(function(){
 
   //IIFE
   (function studyList(){
-    let htmlTitle = ['부트스트랩 개념', '디바이스별 크기', 'col-size-number', '레이아웃 실습', 'offset, pull, push'];
+    let htmlTitle = [
+                      '부트스트랩 개념', '디바이스별 크기', 'col-size-number',
+                      '레이아웃 실습', 'offset, pull, push', 'table',
+                      'button', 'image', '도움이 되는 클래스',
+                      '디바이스 크기별로 요소를 보이고 숨기기', '글리피콘, 드랍다운, 네비게이션'
+                    ];
 
     htmlTitle.forEach(function(e, i) {
       let htmlNumber = i+1;
