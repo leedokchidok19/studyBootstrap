604ce99c3854978ba4ab12d4e2bdc2e0d5ad5411
leedokchidok19 <duckhyun4433@naver.com>
2020-05-01 01:53:55 +0900

    썸네일, 진행바, 패널, 임베드, 모달

--- main/index.js
+++ main/index.js
@@ -7,8 +7,9 @@ $(function(){
                       '부트스트랩 개념', '디바이스별 크기', 'col-size-number',
                       '레이아웃 실습', 'offset, pull, push', 'table',
                       'button', 'image', '도움이 되는 클래스',
-                      '디바이스 크기별로 요소를 보이고 숨기기', '글리피콘, 드랍다운, 네비게이션',
-                      '네비게이션 바, 스크롤스파이','경로(breadcrumbs), 페이지네이션, 라벨(label), 배지, 점보트론(jumbotron)'
+                      '디바이스 크기별로 요소를 보이고 숨기기', '글리피콘, 드랍다운, 네비게이션', '네비게이션 바, 스크롤스파이',
+                      '경로(breadcrumbs), 페이지네이션(pagination), 라벨(label), 배지(dadge), 점보트론(jumbotron)', '썸네일, 진행바', '패널, 반응성 임베디 컴포넌트',
+                      '자바스크립트로 동작하는 요소(모달창)'
                     ];
 
     htmlTitle.forEach(function(e, i) {
