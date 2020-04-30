0e9c1ec5413a10b432ea1e9363050c781b378c02
leedokchidok19 <duckhyun4433@naver.com>
2020-04-30 22:43:46 +0900

    breadcrumbs pagination label badge jumbtron

--- main/index.js
+++ main/index.js
@@ -7,7 +7,8 @@ $(function(){
                       '부트스트랩 개념', '디바이스별 크기', 'col-size-number',
                       '레이아웃 실습', 'offset, pull, push', 'table',
                       'button', 'image', '도움이 되는 클래스',
-                      '디바이스 크기별로 요소를 보이고 숨기기', '글리피콘, 드랍다운, 네비게이션'
+                      '디바이스 크기별로 요소를 보이고 숨기기', '글리피콘, 드랍다운, 네비게이션',
+                      '네비게이션 바, 스크롤스파이','경로(breadcrumbs), 페이지네이션, 라벨(label), 배지, 점보트론(jumbotron)'
                     ];
 
     htmlTitle.forEach(function(e, i) {
